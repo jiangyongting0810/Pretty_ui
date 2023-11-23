bac3dce9d54a042c2a0a16e7fbf81f76f4f48d9a
YongTing <jiangyongting0810@gmail.com>
2023-11-23 16:25:35 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Frank/>
+  </div>
+</template>
+
+<script>
+import Frank from './components/Frank.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Frank
+  }
+}
+</script>
---
+++ src/components/Frank.vue
@@ -0,0 +1,3 @@
+<template>
+  hi11
+</template>
