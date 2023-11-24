fe1715c8e14682ac0f311a2f450d44380640d46f
YongTing <jiangyongting0810@gmail.com>
2023-11-24 14:31:46 +0800

    初始化 aside 的 html

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,11 +1,27 @@
 <template>
-  <div class="topnav">
-      <Topnav/>
-      <ul class="menu">
-        <li>菜单1</li>
-        <li>菜单2</li>
-      </ul>
+  <div>
+    <Topnav />
+    <div class="content">
+      <aside>
+        <h2>组件列表</h2>
+        <ol>
+          <li>
+            <router-link to="/doc/swich">Switch 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/button">Button 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/dialog">Dialog 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/tabs">Tabs 组件</router-link>
+          </li>
+        </ol>
+      </aside>
+      <main>主内容</main>
     </div>
+  </div>
 </template>
 <script lang="ts">
   import Topnav from "../components/Topnav.vue"
