b084a4bb0dd4a6a2e06fda9c89df39cdcf76db68
YongTing <jiangyongting0810@gmail.com>
2023-11-25 11:40:22 +0800

    整体布局 css

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <Topnav />
+  <div class="layout">
+    <Topnav class="nav" />
     <div class="content">
       <aside v-if="menuVisible">
         <h2>组件列表</h2>
@@ -26,37 +26,63 @@
   </div>
 </template>
 <script lang="ts">
-import { inject, Ref } from 'vue'
-  import Topnav from "../components/Topnav.vue"
-  export default {
-    components:{
-      Topnav
-    },
-    setup() {
-      const menuVisible = inject<Ref<boolean>>('menuVisible')
-      return {menuVisible}
-    },
-  }
+import Topnav from "../components/Topnav.vue";
+import { inject, Ref } from "vue";
+export default {
+  components: { Topnav },
+  setup() {
+    const menuVisible = inject<Ref<boolean>>("menuVisible"); // get
+    return { menuVisible };
+  },
+};
 </script>
 <style lang="scss" scoped>
-  aside{
-    background: lightblue;
-    width: 150px;
-    padding: 16px;
-
-    > h2 {
-    margin-bottom: 4px;
-    }
-    > ol {
-      > li {
-        padding: 4px 0;
-      }
-    }
+.layout {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  > .nav {
+    flex-shrink: 0;
+  }
+  > .content {
+    flex-grow: 1;
+    padding-top: 5px;
+    padding-left: 156px;
     @media (max-width: 500px) {
-      position: fixed;
-      top: 0;
-      left: 0;
-      padding-top: 70px;
+      padding-left: 0; 
     }
   }
+}
+.content {
+  display: flex;
+  > aside {
+    flex-shrink: 0;
+  }
+  > main {
+    flex-grow: 1;
+    padding: 16px;
+    background: lightgreen;
+  }
+}
+aside {
+  background: lightblue;
+  width: 150px;
+  padding: 16px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding-top: 70px;
+  height: 100%;
+  > h2 {
+    margin-bottom: 4px;
+  }
+  > ol {
+    > li {
+      padding: 4px 0;
+    }
+  }
+}
+main {
+  overflow: auto;
+}
 </style>
