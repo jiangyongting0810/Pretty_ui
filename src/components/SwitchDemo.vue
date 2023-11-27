10ef87601c1b95a59fa7ed51848ea387017f7e31
YongTing <jiangyongting0810@gmail.com>
2023-11-27 17:27:32 +0800

    优化代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Switch v-model:value="y"/>
+    <Switch v-model:value="bool"/>
   </div>
 </template>
 <script lang="ts">
@@ -11,9 +11,9 @@ export default {
     Switch
   },
   setup(){
-    const y =ref(false)
+    const bool =ref(false)
     return{
-      y
+      bool
     }
   }
 }
