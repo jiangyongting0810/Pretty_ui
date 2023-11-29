5455fb4d7899ce8c8b054fc924a8c77afc68ef93
YongTing <jiangyongting0810@gmail.com>
2023-11-29 13:56:03 +0800

    Dialog 支持自定义 title 和 content

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -4,12 +4,11 @@
     <div class="pretty-dialog-wrapper">
       <div class="pretty-dialog">
         <header>
-          标题
+          <slot name ='title'/>
           <span @click="close" class="pretty-dialog-close"></span>
         </header>
         <main>
-          <p>第一行字</p>
-          <p>第二行字</p>
+          <slot name="content"/>
         </main>
         <footer>
           <Button level="main" @click="ok">OK</Button>
